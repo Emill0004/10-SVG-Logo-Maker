617e62f6a64ab7253d7ff4b7c3b110589f79cf7d
Eric Miller <e.s.miller13125@gmail.com>
2023-04-29 23:25:52 -0600

    added code to write a new .svg file when running node index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Shapes = require('./lib/shapes.js');
+//Shape, Circle, Triangle, Square
+//shapeColor, text, textolor
 
 inquirer
     .prompt([
         {
             type: 'input',
             message: 'Enter three characters.',
-            name: 'txt',
+            name: 'text',
         },
         {
             type: 'input',
             message: 'Enter text color.',
-            name: 'txtColor',
+            name: 'textColor',
         },
         {
             type: 'list',
@@ -26,11 +29,39 @@ inquirer
         },
     ])
     .then((data) => {
-        const filename = `${data.txt.toLowerCase()}.json`;
+        const filename = `${data.text.toLowerCase()}.svg`;
+
+        // if (data.shape === "Circle") {
+        //     const newLogo = new Shapes[1](data.shapeColor, data.text, data.textColor);
+        //     console.log(newLogo);
+        // }else if (data.shape === "Triangle") {
+        //     const newLogo = new Shapes[2](data.shapeColor, data.text, data.textColor);
+        //     console.log(newLogo);
+        // }else if (data.shape === "Square") {
+        //     const newLogo = new Shapes[3](data.shapeColor, data.text, data.textColor);
+        //     console.log(newLogo);
+        // }
         
-        fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-        err ? console.log(err) : console.log('test')
-        );
+
+        if (data.shape === "Circle") {
+            const newLogo = new Shapes[1](data.shapeColor, data.text, data.textColor);
+            const render = newLogo.render();
+            fs.writeFile(filename, render, (err) =>
+            err ? console.log(err) : console.log('Success!')
+            );
+        } else if (data.shape === "Triangle") {
+            const newLogo = new Shapes[2](data.shapeColor, data.text, data.textColor);
+            const render = newLogo.render();
+            fs.writeFile(filename, JSON.stringify(render, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+            );
+        } else if (data.shape === "Square") {
+            const newLogo = new Shapes[3](data.shapeColor, data.text, data.textColor);
+            const render = newLogo.render();
+            fs.writeFile(filename, JSON.stringify(render, null, '\t'), (err) =>
+            err ? console.log(err) : console.log('Success!')
+            );
+        }
     });
 
     /*
